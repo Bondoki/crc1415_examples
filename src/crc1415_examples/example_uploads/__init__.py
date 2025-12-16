cfed1f26492bcfafce304015d055ea62d490a4b4
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2025-12-16 12:50:06 +0100

    + change category to Tutorial CRC1415

--- src/crc1415_examples/example_uploads/__init__.py
+++ src/crc1415_examples/example_uploads/__init__.py
@@ -2,7 +2,7 @@ from nomad.config.models.plugins import ExampleUploadEntryPoint
 
 crc1415_example_upload_entry_point = ExampleUploadEntryPoint(
     title='CRC1415 Tutorial for Plotting and Fitting with Python',
-    category='Examples',
+    category='Tutorial CRC1415',
     description='This upload provides a notebook as a tutorial that demonstrates how to use python for plotting and fitting of data with corporate design of the CRC1415.',
     path='example_uploads/crc1415_tutorial/*',
 )
